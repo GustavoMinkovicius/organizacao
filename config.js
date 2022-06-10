223626692b12ebaaca347aefdb2e175ab78c3665
GustavoMinkovicius <87321967+GustavoMinkovicius@users.noreply.github.com>
2022-06-10 17:39:29 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,19 @@
+import { REACT_APP_API_KEY,
+    REACT_APP_AUTH_DOMAIN,
+    REACT_APP_PROJECT_ID,
+    REACT_APP_STORAGE_BUCKET,
+    REACT_APP_MESSAGING_SENDER_ID,
+    REACT_APP_APP_ID} from "@env"
 import firebase from "firebase";
 require("@firebase/firestore");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDga9M1qDdpe7YiUSqtl_fjej0Vx07kPwQ",
-    authDomain: "organizacao-c796f.firebaseapp.com",
-    projectId: "organizacao-c796f",
-    storageBucket: "organizacao-c796f.appspot.com",
-    messagingSenderId: "210864414775",
-    appId: "1:210864414775:web:8610bab34d6840ed611fb2"
+    apiKey:  REACT_APP_API_KEY,
+    authDomain:  REACT_APP_AUTH_DOMAIN,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket:  REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId:  REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
